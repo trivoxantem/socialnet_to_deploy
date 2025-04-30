305a1a05a6b2cbb2ff1184eb2f61fc08a0172be1
trivoxantem <codewithjosh18@gmail.com>
2025-04-30 06:39:06 -0600

    Update settings.py

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 
@@ -79,11 +80,11 @@ WSGI_APPLICATION = 'socialnet.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://postgres:hERdbiqilMUYEfIwnpjczzoyegLnzgTJ@shinkansen.proxy.rlwy.net:10006/railway"
+    )
 }
+
 
 
 # Password validation
